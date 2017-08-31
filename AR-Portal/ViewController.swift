aef2557b72f546a0670706ef75238d3aa3ed2d93
Rayan Slim <rayanslim@Rayans-MacBook-Pro.local>
2017-08-31 12:28:43 -0400

    1

--- AR-Portal/ViewController.swift
+++ AR-Portal/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var planeDetected: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingSessionConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +28,15 @@ class ViewController: UIViewController {
     }
 
 
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
+    }
+    
 }
 
